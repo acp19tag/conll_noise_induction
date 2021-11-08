29f3eb47f49277059f0c93a15eda489f425e1362
Thomas Green <tafgreen1@sheffield.ac.uk>
2021-11-08 10:08:59 +0000

    Refactoring for publication

--- run_test.py
+++ run_test.py
@@ -2,7 +2,7 @@
 
 # import packages
 from scripts.utils import *
-from noise_induction import NoiseInductor
+from scripts.noise_induction import NoiseInductor
 import json
 
 # load config parameters
@@ -11,15 +11,15 @@ with open("config.json") as json_config_file:
     
 NI = NoiseInductor(config)
 
-# NI.run_noise_induction_model(
-#     noise_type='random',
-#     output_dir='output/random_error.csv'
-# )
+NI.run_noise_induction_model(
+    noise_type='random',
+    output_dir='output/random_error.csv'
+)
 
-# NI.run_noise_induction_model(
-#     noise_type='systematic',
-#     output_dir='output/systematic_error.csv'
-# )
+NI.run_noise_induction_model(
+    noise_type='systematic',
+    output_dir='output/systematic_error.csv'
+)
 
 NI.run_denoise_induction_model(
     output_dir='output/denoise.csv'
